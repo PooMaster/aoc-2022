d3ec7741018f95943db2f026c10271384c2ecfe7
Peter Curtis <peter.d.curtis@gmail.com>
2022-12-09 13:14:04 -0500

    Set up dev environment

---
+++ day_template/main.py
@@ -0,0 +1,58 @@
+"""
+### Part 1:
+
+<paste in problem description here>
+"""
+
+
+def test_part1() -> None:
+    """For example:"""
+    # > `""` results in  `...`.
+    assert part1("") == ...
+
+
+"""
+<end of problem description>
+"""
+
+# === Part 1 Solution: ===
+
+
+def part1(input: str) -> ...:
+    """ """
+    return ...
+
+
+"""
+### Part 2:
+
+<paste in problem description here>
+"""
+
+
+def test_part2() -> None:
+    """For example:"""
+    # > `""` results in  `...`.
+    assert part2("") == ...
+
+
+"""
+<end of problem description>
+"""
+
+# === Part 2 Solution: ===
+
+
+def part2(input: str) -> ...:
+    """ """
+    return ...
+
+
+if __name__ == "__main__":
+    puzzle_input = open("input.txt").read()
+
+    # Print out part 1 solution
+    print("Part 1:", part1(puzzle_input))
+
+    # Print out part 2 solution
+    print("Part 2:", part2(puzzle_input))
