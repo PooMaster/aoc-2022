c8da1e5f938ab8ca7129f9fdf327544a931fcb90
Peter Curtis <peter.d.curtis@gmail.com>
2022-12-09 15:35:29 -0500

    Update day template with text stream input type

--- day_template/main.py
+++ day_template/main.py
@@ -4,11 +4,14 @@
 <paste in problem description here>
 """
 
+import io
+from typing import TextIO
+
 
 def test_part1() -> None:
     """For example:"""
     # > `""` results in  `...`.
-    assert part1("") == ...
+    assert part1(io.StringIO("")) == ...
 
 
 """
@@ -18,7 +21,7 @@ def test_part1() -> None:
 # === Part 1 Solution: ===
 
 
-def part1(input: str) -> ...:
+def part1(input: TextIO) -> ...:
     """ """
     return ...
 
@@ -33,7 +36,7 @@ def part1(input: str) -> ...:
 def test_part2() -> None:
     """For example:"""
     # > `""` results in  `...`.
-    assert part2("") == ...
+    assert part2(io.StringIO("")) == ...
 
 
 """
@@ -43,16 +46,16 @@ def test_part2() -> None:
 # === Part 2 Solution: ===
 
 
-def part2(input: str) -> ...:
+def part2(input: TextIO) -> ...:
     """ """
     return ...
 
 
 if __name__ == "__main__":
-    puzzle_input = open("input.txt").read()
-
     # Print out part 1 solution
-    print("Part 1:", part1(puzzle_input))
+    with open("input.txt") as puzzle_input:
+        print("Part 1:", part1(puzzle_input))
 
     # Print out part 2 solution
-    print("Part 2:", part2(puzzle_input))
+    with open("input.txt") as puzzle_input:
+        print("Part 2:", part2(puzzle_input))
